e67a55bceb37dc39df02b1bf4e63ffa09f1247d1
luizdaniel233 <luizsdossantos@icomp.ufam.edu.br>
2022-10-05 00:07:23 -0400

    UFAM-X:Create db in aws and connection

--- src/database/db.js
+++ src/database/db.js
@@ -1,5 +1,13 @@
 const db = require('mysql2')
-const database = require("./database.json")
+require("dotenv").config();
+
+const database = {
+    "host": process.env.HOST_DATABASE,
+    "port": process.env.PORT_DATABASE,
+    "user": "root",
+    "password": "21092001",
+    "database": process.env.DATABASE
+}
 
 const conexao = db.createConnection(database)
 
