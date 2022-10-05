665eb35ee5046f48162d0828f253c7a9e3fc2429
luizdaniel233 <luizsdossantos@icomp.ufam.edu.br>
2022-10-04 23:49:38 -0400

    UFAM-X:Create db in aws and connection

--- src/server.js
+++ src/server.js
@@ -3,13 +3,11 @@ const express = require('../config/index');
 const app = express();
 const port = 3001;
 
-// db.connect(erro => {
-//     if(erro){
-//         console.log(erro)
-//     }else{
-//         console.log("Database connected!")
-//         app.listen(port,() => console.log(`Port ${port} available!`))
-//     }
-// })
-
-app.listen(port,() => console.log(`Port ${port} available!`))
+db.connect(erro => {
+    if(erro){
+        console.log(erro)
+    }else{
+        console.log("Database connected!")
+        app.listen(port,() => console.log(`Port ${port} available!`))
+    }
+})
